96843b87f466830c3a074b60a1ffd5bc01789717
13adman-aim <ifeoluwaadelakun20@gmail.com>
2025-08-24 05:07:20 +0100

    Update Lecturer.js

--- netlify/functions/models/Lecturer.js
+++ netlify/functions/models/Lecturer.js
@@ -1,1 +1,11 @@
+// netlify/functions/models/Lecturer.js
+import mongoose from "mongoose";
+
+const LecturerSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  staffId: { type: String, required: true, unique: true },
+  password: { type: String, required: true } // store hashed password
+});
+
+export default mongoose.models.Lecturer || mongoose.model("Lecturer", LecturerSchema);
 
