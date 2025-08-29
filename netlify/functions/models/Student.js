d00aceb64559e9702933fb9ba04b2e0294f9a74c
13adman-aim <ifeoluwaadelakun20@gmail.com>
2025-08-29 19:39:21 +0100

    Create Student.js

---
+++ netlify/functions/models/Student.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  matricNo: { type: String, required: true, unique: true }, // like staffId
+  password: { type: String, required: true }
+});
+
+const Student = mongoose.models.Student || mongoose.model("Student", studentSchema);
+
+export default Student;
